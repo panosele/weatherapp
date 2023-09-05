c8539302bf2a8f132d2a22f27e9a2e06d5a7914a
Panos <panosfps1993@gmail.com>
2023-09-05 13:44:33 +0300

    setupa for heroku

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const PORT = port = process.env.PORT || 8080;
+
+let PORT = process.env.PORT;
+if (PORT == null || PORT == "") {
+  PORT = 8000;
+}
+
+
+// const PORT = port = process.env.PORT || 8080;
 const _dirname = "/";
 const API_KEY = process.env.OPENWEATHER_API;
 const UV_API_KEY = process.env.UV_API;
